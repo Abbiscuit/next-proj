05809530d9816951fd0eb678d03602f2613d9c04
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-04-11 22:33:50 +0900

    feat(app): スターターページ追加

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,20 @@
-import { Html, Main, Head, NextScript } from 'next/document';
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import theme from '../src/theme';
 
-export class MyDocument extends Component {
-  static async getInitialProps(context) {
-    const initialProps = await Document.getInitialProps(context);
-    return { ...initialProps };
-  }
+export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head />
+      <Html lang="en">
+        <Head>
+          {/* PWA primary color */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -18,4 +24,22 @@ export class MyDocument extends Component {
   }
 }
 
-export default MyDocument;
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
